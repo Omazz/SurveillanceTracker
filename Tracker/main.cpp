75014728eeeb32e1dff7bd6c4c7a82b7146ea044
Omazz <58297613+Omazz@users.noreply.github.com>
2023-02-14 21:43:59 +0300

    начало написания юнит тестов к читалке

--- Tracker/main.cpp
+++ Tracker/main.cpp
@@ -2,10 +2,14 @@
 #include <includes.h>
 #include <asterixreader.h>
 #include <protocol.h>
+#include <test_asterixreader.h>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    QTest::qExec(new Test_AsterixReader);
+
 
     if(argc != 2) {
         std::cout << "You should write a pcap-file filepath as argument! Try again." << std::endl;
@@ -16,7 +20,7 @@ int main(int argc, char *argv[])
 
     std::list<Asterix34> res34;
     std::list<Asterix48> res48;
-
+    //asterixReader.readPcapFileWithTimeIntervals("/home/leonid/projects/OverviewTracker/Records/record1.pcapng", &res48, &res34, true);
     asterixReader.readPcapFileWithTimeIntervals(argv[1], &res48, &res34, true);
 
     return a.exec();
---
+++ Tracker/test_asterixreader.cpp
@@ -0,0 +1,67 @@
+#include "test_asterixreader.h"
+
+Test_AsterixReader::Test_AsterixReader(QObject *parent)
+    : QObject{parent}
+{
+
+}
+
+void Test_AsterixReader::parseAsterix34_checkField_LEN() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::parseAsterix34_checkField_FSPEC() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::parseAsterix34_checkField_DataSourceIdentifier() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::parseAsterix34_checkField_MessageType() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::parseAsterix34_checkField_TimeOfDay() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::parseAsterix48_checkField_LEN() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::parseAsterix48_checkField_FSPEC() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::parseAsterix48_checkField_DataSourceIdentifier() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::parseAsterix48_checkField_TimeOfDay() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::parseAsterix48_checkField_TargetReportDescriptor() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::parseAsterix48_checkField_RHO() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::parseAsterix48_checkField_THETA() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::asterix34_checkSize_numberRecords() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::asterix48_checkSize_numberRecords() {
+    QCOMPARE(true, true);
+}
+
+void Test_AsterixReader::pcapFile_checkSize_numberRecords() {
+    QCOMPARE(true, true);
+}
