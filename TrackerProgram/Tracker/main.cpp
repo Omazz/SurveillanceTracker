079819e553056ef8b845ff19fdb79473b8f7adf9
Omazz <58297613+Omazz@users.noreply.github.com>
2023-03-15 19:21:54 +0300

    возврат программы трекера

---
+++ TrackerProgram/Tracker/main.cpp
@@ -0,0 +1,16 @@
+#include <QCoreApplication>
+#include "../TrackerFiles/asterixreader.h"
+#include "../TrackerFiles/protocol.h"
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+    if(argc != 2) {
+        std::cout << "You should write a pcap-file filepath as argument! Try again." << std::endl;
+        throw std::exception();
+    }
+    AsterixReader asterixReader;
+    asterixReader.readPcapFileWithTimeIntervals(argv[1], true);
+
+    return a.exec();
+}
